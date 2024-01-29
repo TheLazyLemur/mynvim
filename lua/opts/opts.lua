01ec1dca2bc8a7bf5713a0627f1d025cac30cde3
Dan Rousseau <danrousseau@protonmail.com>
2024-01-29 07:35:45 +0200

    Highlight yanked test

--- lua/opts/opts.lua
+++ lua/opts/opts.lua
@@ -32,3 +32,7 @@ vim.filetype.add({
 		templ = "templ",
 	},
 })
+
+vim.cmd([[
+	autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup=(vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'), timeout=500}
+]])
